b06c1beab12b836a6daeb76eac178aed24dab867
Efim Kashapov <bambalabes@gmail.com>
2024-04-29 14:57:33 +0500

    add func output_text_title and comment

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def output_text_capital(text):
+    """Преобразование строки к верхнему регистру"""
     return text.upper()
+
+
+def output_text_title(text):
+    """ Первую букву каждого слова переводит в верхний регистр"""
+    return text.title()
